b69bd3b2025fb56d26e26bcfc9356f8b7e9656b9
INAKHE ISRAEL <israelinakhe@gmail.com>
2023-06-06 13:48:33 +0100

    Almost

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import {logoDark} from '../assets';
+import {logoDark, cart, logged} from '../assets';
+
 
 const Header = () => {
   return (
-    <div className='w-full h-20 bg-white border-b-[1px] border-b-gray-800 font-titleFont'>
-        <div className='max-w-screen-xl h-full mx-auto flex items-center justify-between'>
+    <div className='w-full h-20 bg-white border-b-[1px]
+     border-b-gray-800 font-titleFont sticky'>
+        <div className='max-w-screen-xl 
+        h-full mx-auto flex items-center justify-between'>
              <div>
                 <img className='w-28' src={logoDark} alt='logo' />
              </div> 
-             <div>
+             <div className='flex items-center gap-8'>
                 <ul className='flex items-center gap-8 '>
                   <li className='text-base text-black font-bold
                    hover:text-orange-900 hover:underline
@@ -29,8 +32,17 @@ const Header = () => {
                   <li className='text-base text-black font-bold
                    hover:text-orange-900 hover:underline
                     underline-offset-2 decoration-[1px] cursor-pointer
-                     duration-300 pr-20'>Blog</li>
+                     duration-300'>Blog</li>
                 </ul>
+                <div className='relative'>
+                   <img className='w-6' src={cart} alt='cart' /> 
+                   <span className='absolute w-6 top-2 left-0 text-sm
+                   flex items-center justify-center font-semibold'>0</span>
+                </div>
+                <img className='w-8 h-8 rounded-full' 
+                src={logged}
+                  alt='userLogo' />
+                
             </div>
         </div>
     </div>
